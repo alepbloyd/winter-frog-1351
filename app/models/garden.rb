26bdeca143df1d60cba34f6b749d5e369197921c
Alex Boyd <alexanderleeboyd@gmail.com>
2022-08-08 13:48:33 -0400

    Final commit

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -6,7 +6,10 @@ class Garden < ApplicationRecord
   def under_100_day_plants
     self.plants
         .select("plants.*")
+        # .select("plants.*, count(plants.plot) AS plot_count")
+        # .group(:id)
         .where("plants.days_to_harvest < ?", 100)
+        # .order(plot_count: :desc)
         .distinct
   end
 end
